e732555065560b3115e78ab75873a0d632cebc2a
LinneaJohansen <linnea_johansen@outlook.com>
2022-09-26 10:34:14 +0200

    Added components and login service

---
+++ src/app/components/catalogue-form/catalogue-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogue-form',
+  templateUrl: './catalogue-form.component.html',
+  styleUrls: ['./catalogue-form.component.css']
+})
+export class CatalogueFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
