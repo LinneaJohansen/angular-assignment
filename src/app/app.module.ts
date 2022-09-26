fa43992d33adb7072e6366d039d1d81f6209bf42
LinneaJohansen <linnea_johansen@outlook.com>
2022-09-26 08:54:57 +0200

    Added skeleton for pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginPage } from './pages/login/login.page';
+import { TrainerPage } from './pages/trainer/trainer.page';
+import { CataloguePage } from './pages/catalogue/catalogue.page';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPage,
+    TrainerPage,
+    CataloguePage
   ],
   imports: [
     BrowserModule
