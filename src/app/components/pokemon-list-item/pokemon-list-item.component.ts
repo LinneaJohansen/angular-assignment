dc8bce3f9c86fe1e1943878e1a249251670b5d25
Karoline Øijorden <karoline.oijorden@no.experis.com>
2022-09-28 15:18:18 +0200

    add pokeball with pokemons that are collected

--- src/app/components/pokemon-list-item/pokemon-list-item.component.ts
+++ src/app/components/pokemon-list-item/pokemon-list-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { Pokemon } from 'src/app/models/pokemon.model';
 import { Trainer } from 'src/app/models/trainer.model';
 import { TrainerService } from 'src/app/services/trainer.service';
@@ -11,17 +11,18 @@ import { TrainerService } from 'src/app/services/trainer.service';
 export class PokemonListItemComponent implements OnInit {
 
   @Input() pokemon?: Pokemon;
+  public isCollected:Boolean = false;
 
   constructor(private readonly trainerService: TrainerService) { }
 
   ngOnInit(): void {
   }
 
-  public collectPokemon(newPokemon: Pokemon): void {
+  public collectPokemon(): void {
     let trainer: Trainer | undefined = this.trainerService.trainer;
-    if (trainer != undefined) {
-      trainer.pokemon.push(newPokemon);
-      alert(newPokemon.name + " was caught");
+    if (trainer != undefined && this.pokemon != undefined && this.isCollected == false) {
+      trainer.pokemon.push(this.pokemon);
+      this.isCollected = true;
     }
   }
 }
